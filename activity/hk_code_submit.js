8edec62c61b4090ad84169d40e05b8a20d433770
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-03-28 15:33:13 +0530

    First Question Opened

--- activity/hk_code_submit.js
+++ activity/hk_code_submit.js
@@ -28,12 +28,30 @@ browserPromise
         return passwordWillBeTypesPromise;
     })
     .then(function () {
-        let loginPageWillBeClickedPromise = gtab.click("button[type='submit']");
-        return loginPageWillBeClickedPromise;
+        let loginPageWillBeClickedPromise = gtab.click("button[data-analytics='LoginPassword']");
+        let IPKitChallengeElementPromise = gtab.waitForSelector(".card-content h3[title = 'Interview Preparation Kit']",{ visible: true });
+        let combinedPromise = Promise.all([loginPageWillBeClickedPromise, gtab.waitForNavigation({waitUntil:"networkidle0" }), IPKitChallengeElementPromise]);
+        return combinedPromise;
     })
     .then(function () {
-        console.log("Login Done");
-        
+        console.log("Login Done IPK element loaded");
+        let interviewPrepKitPromise = gtab.click(".card-content h3[title = 'Interview Preparation Kit']");
+        let WarmUpChallengeElementPromise = gtab.waitForSelector("a[data-attr1 = 'warmup']",{ visible: true });
+        let combinedPromise = Promise.all([interviewPrepKitPromise, gtab.waitForNavigation({waitUntil:"networkidle0" }), WarmUpChallengeElementPromise]);
+        return combinedPromise;
+    })
+    .then(function () {
+        console.log("Interview Prep Kit Page Opened And Warm up Challenge loaded");
+        let warmUpChallengePromise = gtab.click("a[data-attr1 = 'warmup']");
+        let sockMerchantPromise = gtab.waitForSelector("a[data-attr1 = 'sock-merchant']",{ visible: true });
+        let combinedPromise = Promise.all([warmUpChallengePromise, gtab.waitForNavigation({waitUntil:"networkidle0" }), sockMerchantPromise]);
+        return combinedPromise;
+    })
+    .then(function () {
+        console.log("Warm-up Page Opened and Sock merchant loaded");
+        let clickPromise = gtab.click("a[data-attr1 = 'sock-merchant']");
+        let combinedPromise = Promise.all([clickPromise, gtab.waitForNavigation({waitUntil:"networkidle0" })]);
+        return combinedPromise;
     })
     .catch(function (err) {
         console.log(err);
